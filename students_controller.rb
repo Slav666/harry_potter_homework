e9852499280c083eedba92c78260673061338c3d
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-12-12 17:47:06 +0000

    Adding Harry Potter homework

---
+++ students_controller.rb
@@ -0,0 +1,48 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('models/student')
+require_relative('models/house')
+also_reload('./models/*')
+
+# index
+get "/students" do
+ @students = Student.all()
+ erb( :index )
+end
+
+# new
+get '/students/new' do
+  erb( :new )
+end
+
+# show
+get "/students/:id" do
+  @student = Student.find(params[:id].to_i)
+  erb( :show )
+end
+
+# create
+post "/students" do
+  @student = Student.new( params )
+  @student.save()
+  erb( :create )
+end
+
+# edit
+get '/students/:id/edit' do
+  @student = Student.find(params[:id])
+  erb( :edit )
+end
+
+# update
+post '/students/:id' do
+  Student.new(params).update
+  redirect to '/students'
+end
+
+# destroy
+post '/students/:id/delete' do
+  student = Student.find( params[:id].to_i )
+  student.delete()
+  redirect to '/students'
+end
